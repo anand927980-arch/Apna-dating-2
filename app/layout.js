c4e25305cf8c073b1f4830b906c8ef2701485483
anand927980-arch <anand927980@gmail.com>
2026-05-22 03:43:50 +0530

    Create RootLayout component with metadata

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "apna-dating-2",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
